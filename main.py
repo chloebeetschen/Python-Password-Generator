32fc66f71d2340efa17b30aa834f82a0e9d8ebc7
chloebeetschen <chloe.a.b@btinternet.com>
2023-08-16 15:04:45 +0100

    Creation of files to be used

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 
 def main():
     #please select an option... 
-    print("Please select an option you would like to use: ")
+    print("Please select an option/tool you would like to use: ")
     print("1) Password Generator")
     print("2) Password Cracker")
     print("3) Forgotten Password")
+    print("4) Password Manager")
+    print("5) File Recovery")
+    print("6) Password Strength Checker")
+    print("7) Encrypt your password")
     
     choice = int(input("Please enter the option number of your choice: "))
